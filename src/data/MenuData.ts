5dd9e38f6fbcaa53260005f6dcfb312e6bfb2ae4
Weirdmeats <165041244+Weirdmeats@users.noreply.github.com>
2025-10-23 21:24:46 -0300

    tarea-sorpresa-23-10-2025
    tarea-sorpresa-23-10-2025

---
+++ src/data/MenuData.ts
@@ -0,0 +1,14 @@
+interface ItemsMenu {
+    href: string,
+    texto: string,
+
+}
+
+export const MenuData: ItemsMenu[]= [
+    {href: "/", texto: "Home"},
+    {href: "/", texto: "Characters"},
+    {href: "/", texto: "Episodes"}, 
+    {href: "/", texto: "Location"}, 
+
+    
+]
